2c4b85b9b13f4913a2e9abe5825b350ead7fa3ef
lispking <lispking@qq.com>
2024-12-19 09:43:49 +0800

    add ut assert

--- tests/balance_manager_tests.rs
+++ tests/balance_manager_tests.rs
@@ -1,13 +1,19 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, str::FromStr};
 
 use sui_deepbookv3::{
     transactions::balance_manager::BalanceManagerContract,
-    utils::{config::{DeepBookConfig, Environment}, types::BalanceManager},
+    utils::{
+        config::{DeepBookConfig, Environment},
+        types::BalanceManager,
+    },
 };
-use sui_sdk::{types::{
-    base_types::SuiAddress, programmable_transaction_builder::ProgrammableTransactionBuilder,
-}, SuiClientBuilder};
-use utils::{dry_run_transaction, execute_transaction};
+use sui_sdk::{
+    types::{
+        base_types::SuiAddress, programmable_transaction_builder::ProgrammableTransactionBuilder,
+    },
+    SuiClientBuilder,
+};
+use utils::dry_run_transaction;
 
 mod utils;
 
@@ -18,12 +24,14 @@ async fn test_create_and_share_balance_manager() {
     let config = deep_book_config();
     println!("config: {:#?}", config);
 
-    let balance_manager = BalanceManagerContract::new(sui_client, config);
+    let balance_manager = BalanceManagerContract::new(sui_client.clone(), config);
 
     let mut ptb = ProgrammableTransactionBuilder::new();
 
     let _ = balance_manager.create_and_share_balance_manager(&mut ptb);
-    execute_transaction(ptb).await;
+    // execute_transaction(ptb).await;
+    let result = dry_run_transaction(&sui_client, ptb).await.unwrap();
+    println!("result: {:#?}", result);
 }
 
 #[tokio::test]
@@ -40,7 +48,15 @@ async fn test_balance_manager_owner() {
 
     let result = dry_run_transaction(&sui_client, ptb).await.unwrap();
     let result = result.first().unwrap();
-    println!("owner: {:#?}", bcs::from_bytes::<SuiAddress>(&result.0).unwrap());
+    println!(
+        "owner: {:#?}",
+        bcs::from_bytes::<SuiAddress>(&result.0).unwrap()
+    );
+    assert_eq!(
+        bcs::from_bytes::<SuiAddress>(&result.0).unwrap(),
+        SuiAddress::from_str("0x7731f9c105f3c2bde96f0eca645e718465394d609139342f3196383b823890a9")
+            .unwrap()
+    );
 }
 
 #[tokio::test]
@@ -57,16 +73,26 @@ async fn test_balance_manager_id() {
 
     let result = dry_run_transaction(&sui_client, ptb).await.unwrap();
     let result = result.first().unwrap();
-    println!("id: {:#?}", bcs::from_bytes::<SuiAddress>(&result.0).unwrap());
+    println!(
+        "id: {:#?}",
+        bcs::from_bytes::<SuiAddress>(&result.0).unwrap()
+    );
+    assert_eq!(
+        bcs::from_bytes::<SuiAddress>(&result.0).unwrap(),
+        SuiAddress::from_str("0x722c39b7b79831d534fbfa522e07101cb881f8807c28b9cf03a58b04c6c5ca9a")
+            .unwrap()
+    );
 }
 
 fn deep_book_config() -> DeepBookConfig {
-    let balance_managers = HashMap::from([
-        ("DEEP", BalanceManager {
-            address: "0x722c39b7b79831d534fbfa522e07101cb881f8807c28b9cf03a58b04c6c5ca9a".to_string(),
+    let balance_managers = HashMap::from([(
+        "DEEP",
+        BalanceManager {
+            address: "0x722c39b7b79831d534fbfa522e07101cb881f8807c28b9cf03a58b04c6c5ca9a"
+                .to_string(),
             trade_cap: None,
-        }),
-    ]);
+        },
+    )]);
 
     DeepBookConfig::new(
         Environment::Testnet,
